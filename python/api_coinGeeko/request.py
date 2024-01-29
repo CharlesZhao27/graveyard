c3685bd0e54424474c9139388f88ee693a35e09a
Charles Zhao <weibo.zhao@icloud.com>
2024-01-29 22:40:56 +1100

    feat: skeleton code for request class and Todo

---
+++ python/api_coinGeeko/request.py
@@ -0,0 +1,23 @@
+class ApiRequst:
+    def __init__(self, api_key, endpoint, debug):
+        self.api_key = api_key
+        self.endpoint = endpoint
+        self.debug = debug
+
+    def ping():
+        # test endpoint
+        return
+    
+    def simpleAll(): 
+        # fetch latest price of crypto assets by id or contract address
+        return
+    
+    def coins():
+        # list of coins on CoinGeeko and their price, market data, historical data and metadata
+        return
+
+    def autoFetch():
+        # implement auto fetch market data strategy by the time limitation and request limitation
+        return
+
+    
